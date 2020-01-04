295924da07e192d42b46d4a8a96f6810675dce71
dyoachum <dyoachum@gmail.com>
2020-01-04 14:46:19 -0700

    completed objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+ public class Objective3Lab1{
+   public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     System.out.println("Please type a word:");
+     String userInput = scan.nextLine();
+
+     System.out.println( userInput );
+
+   }
+}
