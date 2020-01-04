4e18114a310dd02ea534ba4dd40b7df56608e8c3
dyoachum <dyoachum@gmail.com>
2020-01-03 22:13:20 -0700

    completed objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "David Yoachum";
+    int age = 35;
+    String state = "Missouri";
+
+      System.out.println( "Name: David Yoachum Age: 35  State: Missouri");
+  }
+}
