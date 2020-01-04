4cc4dcd04a198d73b7ed7e79be2c58810b7e303d
dyoachum <59103953+dyoachum@users.noreply.github.com>
2020-01-04 15:03:57 -0700

    Update Objective3Lab2.java

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -4,7 +4,7 @@ public class Objective3Lab2{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
 
-    System.out.print("Whats is your name? ");
+    System.out.print("Whats your name? ");
     String userInput = scan.nextLine();
 
     System.out.println("Hello " + userInput );
