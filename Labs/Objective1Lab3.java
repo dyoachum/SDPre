53874c42f527ff87276c8a4e66108cc638794240
dyoachum <dyoachum@gmail.com>
2020-01-03 20:44:59 -0700

    completed objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3{
+    public static void main(String[] args){
+
+      System.out.println( "Name : David Yoachum" );
+      System.out.println( "Age : 35" );
+      System.out.println( "State : Missouri" );
+
+    }
+}
