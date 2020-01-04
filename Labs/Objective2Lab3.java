5e004774e745c833fea13b36aec49381e44233dc
dyoachum <dyoachum@gmail.com>
2020-01-03 22:34:04 -0700

    completed objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main( String[] args) {
+    String food1, food2, food3;
+
+    System.out.println( " My favorite Foods Are: ");
+    System.out.println( " 1. Pizza" );
+    System.out.println( " 2. Tacos" );
+    System.out.println( " 3. Burgers" );
+  }
+}
