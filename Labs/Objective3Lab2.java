215229355902bd1d740b7c0d9b06e82d1d6e10f5
dyoachum <dyoachum@gmail.com>
2020-01-05 11:44:12 -0700

    completed objective3lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -4,7 +4,7 @@ public class Objective3Lab2{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
 
-    System.out.print("Hello, what is your name? ");
+    System.out.println("Whats your name? ");
     String userInput = scan.nextLine();
 
     System.out.println("Hello " + userInput );
