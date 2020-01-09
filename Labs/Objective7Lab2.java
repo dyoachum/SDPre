21c2002827c2e3d7de7f49031fb1a3c7bf2fa765
dyoachum <dyoachum@gmail.com>
2020-01-09 10:16:37 -0700

    completed Objective7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+public class Objective7Lab2{
+  public static void main(String[] args){
+      System.out.println("In main");
+      printMessage();
+      System.out.println("In main");
+    }
+
+    public static void printMessage(){
+      out.println("In method");
+    }
+  }
