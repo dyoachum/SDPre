b754212031fa5199d01cdbc0fb166f45442d019b
dyoachum <dyoachum@gmail.com>
2020-01-08 22:40:53 -0700

    Completed Objective6Lab3

---
+++ Labs/Objective6Lab3.java
@@ -0,0 +1,14 @@
+public class Objective6Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    while(counter <= 20) {
+      System.out.print(+ counter);
+      if (counter % 2 == 0){
+        System.out.println(" is even ");
+      }
+      else System.out.println(" is odd ");
+      counter = counter + 1;
+    }
+  }
+}
