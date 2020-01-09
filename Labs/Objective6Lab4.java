f9d8a5af13fc7c2b8cde8df3f7fe61c863ff2407
dyoachum <dyoachum@gmail.com>
2020-01-09 00:10:15 -0700

    completed Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,13 @@
+public class Objective6Lab4{
+  public static void main(String[] args){
+    int count , sum;
+
+    count = 0;
+    sum = 0;
+      while (count <= 209){
+        sum = sum + count;
+        count++;
+      }
+      System.out.println(+ count);
+    }
+}
