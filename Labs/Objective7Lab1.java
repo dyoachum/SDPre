afe305fbb09ffb27e289f6b6b0f50234f775e728
dyoachum <dyoachum@gmail.com>
2020-01-09 10:08:07 -0700

    completed Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+public class Objective7Lab1{
+  public static void main(String[] args){
+    printName();
+  }
+  public static void printName() {
+    out.println("Hello Doug");
+
+  }
+}
